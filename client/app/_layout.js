4e6dbf9a6979f991e37c741fd19bbfad37dd9a38
IRSHIT033 <mukherjeeirshit50@gmail.com>
2023-04-04 21:01:29 +0530

    feat: added new sections

--- client/app/_layout.js
+++ client/app/_layout.js
@@ -7,7 +7,7 @@ SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
   const [fontsLoaded] = useFonts({
-    DMBlod: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
     DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
     DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
   });
